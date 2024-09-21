ffde7d60d572a688f2ec8c9318424bcedf6148bf
puly <55434233+dida0701@users.noreply.github.com>
2024-09-22 01:05:58 +0800

    chore: init fe (#2)

---
+++ frontend/src/theme.ts
@@ -0,0 +1,42 @@
+"use client";
+import { Roboto } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#E77F6C",
+      dark: "#E46B55",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#F7DECD",
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+    button: {
+      textTransform: "none",
+    },
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.severity === "info" && {
+            backgroundColor: "#60a5fa",
+          }),
+        }),
+      },
+    },
+  },
+});
+
+export default theme;
