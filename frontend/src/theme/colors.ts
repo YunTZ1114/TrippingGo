d4eae0bbaed99f4bd37f708a1c214d9b7b2f2fa6
Eva <e037l862712@gmail.com>
2024-09-29 03:47:42 +0800

    chore: env setting

---
+++ frontend/src/theme/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  primary: "#E77F6C",
+  surface: "#F7DECD",
+  surfaceLight: "#F7F0EA",
+  white: "#FFFFFF",
+  text: "#000000",
+};
+
+export type ColorType = keyof typeof colors;
