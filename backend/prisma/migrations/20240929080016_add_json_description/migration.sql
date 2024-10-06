7c7221ee2f6a856d3ee006bdbed8c1ac1e68d974
rex20240612 <rex.rex022534@gmail.com>
2024-10-06 16:58:07 +0800

    [refactor] remove /prisma/migrations

--- backend/prisma/migrations/20240929080016_add_json_description/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `description` column on the `trip_members` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "trip_members" DROP COLUMN "description",
-ADD COLUMN     "description" JSONB;
