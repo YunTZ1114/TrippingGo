7c968979daf16393006d16457aa72dfc748755c7
Eva <e037l862712@gmail.com>
2024-10-08 23:06:05 +0800

    feat: add trip template

--- frontend/src/app/trips/[tripId]/layout.tsx
+++ frontend/src/app/trips/[tripId]/layout.tsx
@@ -12,7 +12,7 @@ const Layout = ({
     <div className="w-full flex flex-col h-screen bg-primary">
       <div className="flex-1 flex gap-2 mt-4">
         <SideBar />
-        <div className="p-10 h-full flex-1 rounded-tl-3xl bg-surface shadow-paper">
+        <div className="h-full flex-1 rounded-tl-3xl bg-surface shadow-paper">
           {children}
         </div>
       </div>
--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,11 +1,16 @@
 import { MaterialSymbol } from "@/components/MaterialSymbol";
+import { classNames } from "@/utils";
 
-export const Loading = () => {
+export const Loading = ({
+  colorClass = "text-gray-200",
+}: {
+  colorClass?: string;
+}) => {
   return (
     <div className="h-full w-full flex-1 flex items-center justify-center">
       <MaterialSymbol
         icon="progress_activity"
-        className="text-gray-200 animate-spin"
+        className={classNames(colorClass, "animate-spin")}
         size={72}
       />
     </div>
