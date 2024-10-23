f5effbf5c0d1678ef967457ce0e84d447228e2e2
rex20240612 <rex.rex022534@gmail.com>
2024-10-23 21:58:30 +0800

    feat(be): api get trip-detail

--- backend/src/trips/trip.service.ts
+++ backend/src/trips/trip.service.ts
@@ -26,6 +26,27 @@ export class TripService {
     return trip.id;
   }
 
+  async getTrip(tripId: number) {
+    const tripDetail = await this.databaseService.trip.findUnique({
+      where: {
+        isDeleted: false,
+        id: tripId,
+      },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        coverUrl: true,
+        creatorId: true,
+        currencyCode: true,
+        startTime: true,
+        endTime: true,
+      },
+    });
+
+    return tripDetail;
+  }
+
   async getTrips(filter: TripFilterType, q: string, userId: number): Promise<TripPreview[]> {
     const trips = await this.databaseService.trip.findMany({
       where: {
