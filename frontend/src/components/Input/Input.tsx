4d63d9637b454cb3c50fd0a881374565f0651c5b
Eva <e037l862712@gmail.com>
2024-10-23 23:19:27 +0800

    feat: add setting page

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -2,11 +2,19 @@ import { Input as AntInput, InputProps as AntInputProps, InputRef } from "antd";
 import { forwardRef } from "react";
 
 export interface InputProps extends AntInputProps {}
-
 export const Input = forwardRef(
   ({ ...props }: InputProps, ref: React.LegacyRef<InputRef>) => {
     return <AntInput ref={ref} {...props} />;
   },
 );
-
 Input.displayName = "Input";
+
+export interface TextAreaProps
+  extends React.ComponentProps<typeof AntInput.TextArea> {}
+
+export const TextArea = forwardRef(
+  ({ ...props }: TextAreaProps, ref: React.LegacyRef<InputRef>) => {
+    return <AntInput.TextArea ref={ref} {...props} />;
+  },
+);
+TextArea.displayName = "TextArea";
